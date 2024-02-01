89ed3d6091ff365f1aef658116065ee9728268da
midCenturyDude7 <mgriffes@gmail.com>
2024-01-31 21:05:07 -0800

    Initial commit | project structure/org

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,8 +16,7 @@ export default function Index() {
           <a
             target="_blank"
             href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             15m Quickstart Blog Tutorial
           </a>
         </li>
@@ -25,8 +24,7 @@ export default function Index() {
           <a
             target="_blank"
             href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             Deep Dive Jokes App Tutorial
           </a>
         </li>
